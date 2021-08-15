4097e089aa10baa1cc8e7b3b0d4a486b7e504bcb
Moses Netshitangani <netshitanganimoses@gmail.com>
2021-08-15 16:35:54 +0200

    seek time processed

--- src/components/video.js
+++ src/components/video.js
@@ -5,10 +5,41 @@ import "../style/video.css";
 
 const Video = () => {
 
+    // time elapsed
+    let time = 0;
+    let timeString = "";
+
+    // ref to access the ReactPlayer
     const b = useRef(null);
 
+    // process the time elapsed
+    const processTime = t => {
+        // minutes
+        let mins = Math.floor(t/60);
+        if(mins < 10)
+        {
+            mins = `0${mins}`
+        }
+
+        // seconds
+        let secs = (t % 60);
+        if(secs < 10.0)
+        {
+            secs = `0${secs.toString().substring(0,1)}`;
+        }else
+        {
+            secs = secs.toString().substring(0,2);
+        }
+
+        // time
+        timeString = `[${mins}:${secs}]`;
+    }
+
+    // get time elapsed
     const getElapsed = () => {
-        alert(b.current.getCurrentTime());
+        time = b.current.getCurrentTime();
+        processTime(time);
+        alert(timeString);
     }
 
     return(
