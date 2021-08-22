36fd898531009c0433d4894137a0b3de98f09390
Moses Netshitangani <netshitanganimoses@gmail.com>
2021-08-22 19:55:26 +0200

    quiz template data collected

--- src/components/admin.js
+++ src/components/admin.js
@@ -7,31 +7,62 @@ const Admin = () => {
     // ref to access the ReactPlayer
     const player = useRef(null);
 
-    // get video length
-    const [len, changeLen] = useState('0');
-
-
+    // change video URL
     const [url, OnUrl] = useState("https://");
 
-    const fetchVid = link => {
-        console.log(link.target)
+    // states for quiz
+    const [link, setLink] = useState('');
+    const [ques, setQues] = useState('');
+    const [optA, setOptA] = useState('');
+    const [optB, setOptB] = useState('');
+    const [optC, setOptC] = useState('');
+    const [optD, setOptD] = useState('');
+    const [corr, setCorr] = useState('');
+    const [time, setTime] = useState('');
+
+    
+
+    // methods encapsulating state change
+    let changeLink = e => setLink(e.target.value);
+    let changeQues = e => setQues(e.target.value);
+    let changeOA = e => setOptA(e.target.value);
+    let changeOB = e => setOptB(e.target.value);
+    let changeOC = e => setOptC(e.target.value);
+    let changeOD = e => setOptD(e.target.value);
+    let changeOCorr = e => setCorr(e.target.value);
+    let changeTime = e => setTime(e.target.value);
+
+
+    // quiz structure
+    let quizTemp = {
+        link: link,
+        question: ques,
+        optA: optA,
+        optB: optB,
+        optC: optC,
+        optD: optD,
+        correct: corr,
+        time: time
+    }
+    const createQuiz = () => {
+        console.log(quizTemp);
     }
 
     const getLink = () => {
         OnUrl(document.getElementById('link').value);
-        changeLen(`${player.current.getDuration()}`);
     }
 
     return (
         <div>
-            <h3>Admin Page</h3>
+            <h3>Administrator Page</h3>
             
             <div className='admin-cover'>
                 {/* Lecture Video */}
                 <div className='create-video'>
 
                     <div id='submit-link'>
-                        <input type='text' id='link' placeholder='Lecture video YouTube link'/>
+                        <input type='text' id='link' onChange={e => setLink(e.target.value)}
+                        placeholder='Lecture video YouTube link'/>
                         <div onClick={getLink} className='button-cover c'>Link Video</div>
                     </div>
 
@@ -43,17 +74,18 @@ const Admin = () => {
                 {/* Create Quiz */}
                 <div className="create-cover">
 
-                    <div className='quiz-template'>
-                        <input type='text' placeholder='Type in quiz question' id='q' />
-                        <input type='text' placeholder='Option A' />
-                        <input type='text' placeholder='Option B' />
-                        <input type='text' placeholder='Option C' />
-                        <input type='text' placeholder='Option D' />
-                        <input type='text' placeholder='Correct Option' />
-                        <input type='text' placeholder='Time in [mm:ss] format' id='t' required />
-                    </div>
+                    <form className='quiz-template'>
+                        <input type='text' placeholder='Type in quiz question' 
+                        onChange={e => {setQues(e.target.value)}} id='q' />
+                        <input type='text' placeholder='Option A' onChange={e => {setOptA(e.target.value)}}/>
+                        <input type='text' placeholder='Option B' onChange={e => {setOptB(e.target.value)}}/>
+                        <input type='text' placeholder='Option C' onChange={e => {setOptC(e.target.value)}}/>
+                        <input type='text' placeholder='Option D' onChange={e => {setOptD(e.target.value)}}/>
+                        <input type='text' placeholder='Correct Option' onChange={e => {setCorr(e.target.value)}}/>
+                        <input type='text' placeholder='Time in [mm:ss] format' id='t' onChange={e => {setTime(e.target.value)}} />
+                    </form>
                     
-                    <div className="button-cover c">
+                    <div className="button-cover c" onClick={createQuiz}>
                         Create Quiz
                     </div>
 
