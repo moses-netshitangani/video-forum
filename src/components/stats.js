22390694cc4af1301c00dab1e91ea9df96768d49
Moses Netshitangani <netshitanganimoses@gmail.com>
2021-08-30 12:23:00 +0200

    pie chart init

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
 import '../style/stats.css';
 
 const Stats = props => {
@@ -6,19 +7,37 @@ const Stats = props => {
     return(
         <div className={`stats-cover`}>
 
-            {/* question */}
-            <div className="question">
-                <h4>What is a variable?</h4>
-            </div>
+            <h2>Quiz Statistics</h2>
 
-            {/* bars */}
-            <div className='bar-cover'>
-                <div className='bar a'>Option A</div>
-                <div className='bar b'>Option B</div>
-                <div className='bar c'>Option C</div>
-                <div className='bar d'>Option D</div>
-            </div>
+            
 
+            <div className="chart">
+                <PieChart
+                    data={[
+                        { title: 'One', value: 10, color: 'rgba(43, 122, 226, 0.082)' },
+                        { title: 'Two', value: 15, color: 'rgb(147, 184, 201)' },
+                        { title: 'Three', value: 20, color: 'rgba(43, 122, 226, 0.507)' },
+                        { title: 'Four', value: 55, color: 'rgba(43, 122, 226, 0.734)' }
+                    ]}
+                    lineWidth="60" animate={true} animationDuration="2000"
+                    label={({ x, y, dx, dy, dataEntry }) => (
+                        <text
+                          x={x}
+                          y={y}
+                          dx={dx}
+                          dy={dy}
+                          dominant-baseline="central"
+                          text-anchor="middle"
+                          style={{
+                            fontSize: '5px',
+                            fontFamily: 'sans-serif',
+                          }}
+                        >
+                          {Math.round(dataEntry.percentage) + '%'}
+                        </text>
+                      )}
+                />
+            </div>
         </div>
     )
 }
