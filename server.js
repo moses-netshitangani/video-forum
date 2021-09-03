dce2e37426dd43bef0d9a2af36224d369f5f0b84
Moses Netshitangani <netshitanganimoses@gmail.com>
2021-09-03 16:15:16 +0200

    minor update

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/stats', statsRouter);
 // serve static assets
 if(process.env.NODE_ENV === 'production')
 {
-    app.use(express.static('build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
