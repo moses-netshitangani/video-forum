48e59241aa181d86c268e5dc82ddfad19813ca08
Moses Netshitangani <netshitanganimoses@gmail.com>
2021-09-04 14:22:23 +0200

    disqus update

--- client/src/components/forum.js
+++ client/src/components/forum.js
@@ -5,29 +5,33 @@ import "../style/forum.css";
 const Forum = props => {
 
     const [id, idChange] = useState("");
+    const [url, urlChange] = useState("");
 
     useEffect(() => {
         // Obtaining article id for display
         if(id === "")
+        {
             idChange(props.id);
+            urlChange(`https://pacific-taiga-25458.herokuapp.com/${id}#!newthread`);
+        }
     }, [id, props.id]);
 
     // disqus setup
     const disqusShortname = "tsugi-1";
     const disqusConfig = {
-      url: "https://pacific-taiga-25458.herokuapp.com/#!newthread",
+      url: url,
       identifier: `${id}`,
       title: "OnlineSuccess",
       language: 'en_US'
     }
 
     return(
+        (id === "") ? <div>Loading</div> :
         <div className={`forum-cover ${props.cforum}`}>
             <Disqus.DiscussionEmbed
                 shortname={disqusShortname}
                 config={disqusConfig}
             />
-            
         </div>
     )
 }
